649fee5ce393745ac4a70a037773a3758cc89111
ThePythonDev <22910497+ThePythonDev@users.noreply.github.com>
2018-12-26 17:12:04 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
-"a package by ThePythonDev w/ useful functions / modules"
+"a package by ThePythonDev w/ useful modules"
 
 from custom_module import chronometre, file_manager, graph, utilities, encryption
