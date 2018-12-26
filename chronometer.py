d1d3257b9e4a99ad0f34077831d37222e59228f4
ThePythonDev <22910497+ThePythonDev@users.noreply.github.com>
2018-12-26 17:25:34 +0100

    Update and rename chronometre.py to chronometer.py

---
+++ chronometer.py
@@ -0,0 +1,39 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+"chronometer w/ laps (for laps just enter stop() other times)"
+
+import os
+
+def start():
+	"""
+	start chronometer
+	(return the system's time (needed to stop the chrono))
+	"""
+	begin = [
+		int(time.strftime("%H")), # fetch system's hour
+		int(time.strftime("%M")), # .............. minutes
+		int(time.strftime("%S"))  # .............. seconds
+	]
+	return begin
+	
+def stop(begin):
+	"stop Chronometer"
+	
+	end = [
+		int(time.strftime("%H")), # fetch system's hour
+		int(time.strftime("%M")), # .............. minutes
+		int(time.strftime("%S"))  # .............. seconds
+	]
+	chrono = [ # calculating the time difference between begin and end
+		end[0] - begin[0],
+		end[1] - begin[1],
+		end[2] - begin[2]
+	]
+	while chrono[2] < 0: # time can't be negative so reformating
+		chrono[2] += 60
+		chrono[1] -= 1
+	while chrono[1] < 0:
+		chrono[1] -= 60
+		chrono[0] += 1
+	return end, chrono
+	
