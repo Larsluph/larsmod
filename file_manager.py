1bdefd02cc348b20b6f647bebb47dd0ab675fb35
ThePythonDev <22910497+ThePythonDev@users.noreply.github.com>
2018-12-27 21:24:15 +0100

    Add files via upload

--- file_manager.py
+++ file_manager.py
@@ -9,60 +9,37 @@ def prefix_delete(path, prefix):
 	"delete same prefix of mass files in one folder"
 	
 	files = os.listdir(path) # create a list with all the filenames in folder 'path'
-	for x in files:
-		if x[:len(prefix)] == prefix:
-			os.rename(path + "\\" + x, path + "\\" + x[len(prefix) :])
+	for current in files:
+		if current[:len(prefix)] == prefix:
+			os.rename(path + "\\" + current, path + "\\" + current[len(prefix) :])
 
 def suffix_delete(path, suffix):
 	"delete same suffix of mass files in one folder"
 	
-	fichiers = os.listdir(path) # create a list with all the filenames in folder 'path'
-	for x in fichiers:
-		if os.path.splitext(x)[0][len(os.path.splitext(x)[0]) - len(suffix) :] == suffix:
-			os.rename(path + "\\" + x, path + "\\" + os.path.splitext(x)[0][: len(os.path.splitext(x)[0]) - len(suffix)] + os.path.splitext(x)[1])
+	files = os.listdir(path) # create a list with all the filenames in folder 'path'
+	for current in files:
+		if os.path.splitext(current)[0][len(os.path.splitext(current)[0]) - len(suffix) :] == suffix:
+			os.rename(path + "\\" + current, path + "\\" + os.path.splitext(current)[0][: len(os.path.splitext(current)[0]) - len(suffix)] + os.path.splitext(current)[1])
 	
 def char_delete(path, char):
 	"delete filename up to a certain character"
 	
-	fichiers = os.listdir(path) # create a list with all the filenames in folder 'path'
-	for x in fichiers:
-		for index in range(len(x)):
+	files = os.listdir(path) # create a list with all the filenames in folder 'path'
+	for current in files:
+		for index in range(len(current)):
 			if x[index] == char:
-				os.rename(path + "\\" + x, path + "\\" + x[index+1:])
+				os.rename(path + "\\" + current, path + "\\" + current[index+1:])
 				break
 	
 def char_nbr_delete(path, char_nbr):
 	"delete filename up to a certain number of character"
 	
-	fichiers = os.listdir(path) # create a list with all the filenames in folder 'path'
-	for x in fichiers:
-		for index in range(len(x)):
+	files = os.listdir(path) # create a list with all the filenames in folder 'path'
+	for current in files:
+		for index in range(len(current)):
 			if index == char_nbr:
 				try:
-					os.rename(path + "\\" + x, path + "\\" + x[index+1:])
+					os.rename(path + "\\" + current, path + "\\" + current[index+1:])
 				except FileExistsError:
-					print("Le fichier %s existe déjà!\nskipping..." % (x))
+					raise ("Unable to rename file '{}' because the file already exists. skipping...".format(current) )
 				break
-
-''' In progress
-
-def shift_filename_folder(path):
-	"shift the first letter of each word in all of the filenames in one folder"
-	
-	filelist = os.listdir(path) # create a list with all the filenames in folder 'path'
-	for file in filelist:
-		filename = file.split(" ")
-		new_file = list()
-		new_filename = str()
-
-		print(filelist, file, filename, new_file, new_filename)
-
-		for word in filename:
-			try:
-				word = word[0].upper() + word[1:].lower()
-			except:
-				pass
-			new_file.append(word)
-		new_filename = " ".join(new_file)
-		os.rename(path + "\\" + file, path + "\\" + new_filename)
-'''
