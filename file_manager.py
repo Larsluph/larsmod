7d1020fa96165fc2a0db25ae1c7e4267a4aac328
ThePythonDev <22910497+ThePythonDev@users.noreply.github.com>
2018-12-27 10:36:54 +0100

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -8,36 +8,33 @@ import os
 def prefix_delete(path, prefix):
 	"delete same prefix of mass files in one folder"
 	
-	fichiers = os.listdir(path)
-	for x in fichiers:
+	files = os.listdir(path) # create a list with all the filenames in folder 'path'
+	for x in files:
 		if x[:len(prefix)] == prefix:
 			os.rename(path + "\\" + x, path + "\\" + x[len(prefix) :])
-	os.system("start " + path)
 
 def suffix_delete(path, suffix):
 	"delete same suffix of mass files in one folder"
 	
-	fichiers = os.listdir(path)
+	fichiers = os.listdir(path) # create a list with all the filenames in folder 'path'
 	for x in fichiers:
 		if os.path.splitext(x)[0][len(os.path.splitext(x)[0]) - len(suffix) :] == suffix:
 			os.rename(path + "\\" + x, path + "\\" + os.path.splitext(x)[0][: len(os.path.splitext(x)[0]) - len(suffix)] + os.path.splitext(x)[1])
-	os.system("start " + path)
 	
 def char_delete(path, char):
 	"delete filename up to a certain character"
 	
-	fichiers = os.listdir(path)
+	fichiers = os.listdir(path) # create a list with all the filenames in folder 'path'
 	for x in fichiers:
 		for index in range(len(x)):
 			if x[index] == char:
 				os.rename(path + "\\" + x, path + "\\" + x[index+1:])
 				break
-	os.system("start " + path)
 	
 def char_nbr_delete(path, char_nbr):
 	"delete filename up to a certain number of character"
 	
-	fichiers = os.listdir(path)
+	fichiers = os.listdir(path) # create a list with all the filenames in folder 'path'
 	for x in fichiers:
 		for index in range(len(x)):
 			if index == char_nbr:
@@ -46,12 +43,13 @@ def char_nbr_delete(path, char_nbr):
 				except FileExistsError:
 					print("Le fichier %s existe déjà!\nskipping..." % (x))
 				break
-	os.system("start " + path)
-	
+
+''' In progress
+
 def shift_filename_folder(path):
 	"shift the first letter of each word in all of the filenames in one folder"
 	
-	filelist = os.listdir(path)
+	filelist = os.listdir(path) # create a list with all the filenames in folder 'path'
 	for file in filelist:
 		filename = file.split(" ")
 		new_file = list()
@@ -67,4 +65,4 @@ def shift_filename_folder(path):
 			new_file.append(word)
 		new_filename = " ".join(new_file)
 		os.rename(path + "\\" + file, path + "\\" + new_filename)
-	
+'''
